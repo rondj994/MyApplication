0dbe6153850fbe26a2ae92a60ad6e785aac46cd9
daviderondana <rondanadavide@icloud.com>
2017-05-23 15:59:42 +0200

    collegamento con catalogo: fatto

--- app/src/main/java/com/example/daviderondana/myapplication/Activities/MainActivity.java
+++ app/src/main/java/com/example/daviderondana/myapplication/Activities/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
 
+import com.example.daviderondana.myapplication.ClickListeners.CatalogoClickListener;
 import com.example.daviderondana.myapplication.ClickListeners.LoginClickListener;
 import com.example.daviderondana.myapplication.R;
 
@@ -27,5 +28,17 @@ public class MainActivity extends AppCompatActivity {
 
         // associo il click listener al bottone
         loginButton.setOnClickListener(loginClickListener);
+
+
+
+        // identifico il bottone dal layout
+
+        Button catalogoButton = (Button) findViewById(R.id.catalogo_button);
+
+        // creo un click listener
+        CatalogoClickListener catalogoClickListener = new CatalogoClickListener(activity);
+
+        // associo il click listener al bottone
+        catalogoButton.setOnClickListener(catalogoClickListener);
     }
 }
---
+++ app/src/main/java/com/example/daviderondana/myapplication/ClickListeners/CatalogoClickListener.java
@@ -0,0 +1,21 @@
+package com.example.daviderondana.myapplication.ClickListeners;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.view.View;
+
+import com.example.daviderondana.myapplication.Activities.CatalogoActivity;
+
+public class CatalogoClickListener implements View.OnClickListener {
+    private Activity activity;
+
+    public CatalogoClickListener(Activity activity) {
+        this.activity = activity;
+    }
+
+    @Override
+    public void onClick(View view) {
+        Intent intent = new Intent(activity, CatalogoActivity.class);
+        activity.startActivity(intent);
+    }
+}
